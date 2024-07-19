14342edfad1af2f4357fed0d098ec2413329f693
DhruveshNarigara <dm.narigara@gmail.com>
2024-07-19 20:39:49 +0530

    Add project files.

---
+++ Dhruvesh_Practical_MM/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Dhruvesh_Practical_MM.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Dhruvesh_Practical_MM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
